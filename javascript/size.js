34f6f31e26371c05032af2444de8e52df29d04e4
Kirk Longuski <longuskikirk@gmail.com>
2015-12-08 14:52:33 -0500

    commented out size.js

--- javascript/size.js
+++ javascript/size.js
@@ -1,11 +1,16 @@
+//not necessarily a catch all, just something I use for my own sanity
 function screenSize(mobile, smallDesktop, medDesktop)
 {
     var mobileSize = mobile == undefined ? 980 : mobile;
     var desktopSmall = smallDesktop == undefined ? 1000 : smallDesktop;
     var desktopMed = medDesktop == undefined ? 1500 : medDesktop;
+    //sizes that correspond to what I need them too, mobile is probably what should be adjusted more often
+    //TODO:Add breakpoints for common mobile devices, specify type to device
     var size = "", type = "";
     var body = document.getElementsByTagName('body')[0];
+    //used to do this by ID, but asking people to add IDs to their body is just dumb
     var width = body.offsetWidth;
+    //offsetWidth gives a generally accurate view, certainly good enough for just CSS
     if(width <= mobileSize) type = "mobile";
     else
     {
@@ -18,4 +23,5 @@ function screenSize(mobile, smallDesktop, medDesktop)
         else size = "large-screen";
     }
     body.className = body.className + " " + type + " " + size;
+    //preserves already set classnames!
 }
